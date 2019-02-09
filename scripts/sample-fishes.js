faf443e6f423cbaff4c0e50e7c6d2c86523d5644
nath <nhatty94@gmail.com>
2019-02-09 10:03:09 +0100

    initial commit

---
+++ scripts/sample-fishes.js
@@ -0,0 +1,74 @@
+// This is just some sample data so you don't have to think of your own!
+module.exports = {  
+  fish1 : {
+    name : 'Pacific Halibut',
+    image : 'https://cdn0.wideopeneats.com/wp-content/uploads/2016/12/days-order-seafood-restaurant.png',
+    desc : 'Everyones favorite white fish. We will cut it to the size you need and ship it.',
+    price : 1724,
+    status : 'available'
+  },
+  
+  fish2 : {
+    name : 'Lobster',
+    image : 'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_960_720.jpg',
+    desc : 'These tender, mouth-watering beauties are a fantastic hit at any dinner party.',
+    price : 3200,
+    status : 'available'
+  },
+  
+  fish3 : {
+    name : 'Sea Scallops',
+    image : 'https://cdn.pixabay.com/photo/2016/03/05/23/01/prawn-1239427__340.jpg',
+    desc : 'Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound',
+    price : 1684,
+    status : 'unavailable'
+  },
+  
+  fish4 : {
+    name : 'Mahi Mahi',
+    image : 'https://cdn.pixabay.com/photo/2016/03/05/20/07/asian-1238668__340.jpg',
+    desc : 'Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ',
+    price : 1129 ,
+    status : 'available'
+  },
+  
+  fish5 : {
+    name : 'King Crab',
+    image : 'https://cdn.pixabay.com/photo/2018/12/21/22/07/salmon-3888831__340.jpg',
+    desc : 'Crack these open and enjoy them plain or with one of our cocktail sauces',
+    price : 4234,
+    status : 'available'
+  },
+  
+  fish6 : {
+    name : 'Atlantic Salmon',
+    image : 'https://cdn.pixabay.com/photo/2013/07/19/00/18/seafood-165220__340.jpg',
+    desc : 'This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!',
+    price : 1453,
+    status : 'available'
+  },
+  
+  fish7 : {
+    name : 'Oysters',
+    image : 'https://cdn.pixabay.com/photo/2017/02/14/17/23/united-kingdom-2066475_960_720.jpg',
+    desc : 'A soft plump oyster with a sweet salty flavor and a clean finish.',
+    price : 2543,
+    status : 'available'
+  },
+  
+  fish8 : {
+    name : 'Mussels',
+    image : 'https://cdn.pixabay.com/photo/2016/03/05/22/23/asian-1239271__340.jpg',
+    desc : 'The best mussels from the Pacific Northwest with a full-flavored and complex taste.',
+    price : 425,
+    status : 'available'
+  },
+  
+  fish9 : {
+    name : 'Jumbo Prawns',
+    image : 'https://cdn.pixabay.com/photo/2016/03/05/22/33/cooked-1239316__340.jpg',
+    desc : 'With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.',
+    price : 2250,
+    status : 'available'
+  }
+};
